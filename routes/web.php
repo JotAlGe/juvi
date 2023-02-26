bb22f864abf9ecab8c5744e4d56d73ac06ec77cd
Juanchis <juanchismo10@gmail.com>
2023-02-26 01:55:50 -0300

    Instalación y configuración de laravel inertia js - vue 3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home', [
+        'users' => User::all()
+    ]);
 });
